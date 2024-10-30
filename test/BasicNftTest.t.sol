310857751315fecda7ae19d97e7d234dd483843e
James Kirawan <jameskirawan@gmail.com>
2024-10-30 22:13:06 +0700

    init NFT project

---
+++ test/BasicNftTest.t.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+import {BasicNft} from "src/BasicNft.sol";
+import {DeployBasicNft} from "script/DeployBasicNft.s.sol";
+
+contract BasicNftTest is Test {
+    DeployBasicNft public deployer;
+    BasicNft public basicNft;
+    address public USER = makeAddr("USER");
+    string public constant SHIBA_INU =
+        "ipfs://QmZByEUKqXdr5sYwHrcJB2133VtHuzB8riQsmZnL6T6j2X/?filename=file-ipfs.json";
+
+    function setUp() public {
+        deployer = new DeployBasicNft();
+        basicNft = deployer.run();
+    }
+
+    function testNameIsCorrect() public view {
+        string memory actualName = "BasicNft";
+        string memory expectedName = basicNft.name();
+
+        assert(
+            keccak256(abi.encodePacked(actualName)) ==
+                keccak256(abi.encodePacked(expectedName))
+        );
+    }
+
+    function testCanMintAndHaveABalance() public {
+        vm.prank(USER);
+        basicNft.mintNft(SHIBA_INU);
+
+        assert(basicNft.balanceOf(USER) == 1);
+        assert(
+            keccak256(abi.encodePacked(basicNft.tokenURI(0))) ==
+                keccak256(abi.encodePacked(SHIBA_INU))
+        );
+    }
+}
